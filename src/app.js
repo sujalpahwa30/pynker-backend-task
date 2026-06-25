657be8e05aeba7b6664ebc0638e6ec6d75481452
Sujal Pahwa <sujalpahwa4@gmail.com>
2026-06-25 22:59:21 +0530

    setup the backend server with express

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Backend API is running"
+  });
+});
+
+module.exports = app;
